b000db14eccd3f800fda7745d33da7d628f0440a
yohannes <joeabrha@gmail.com>
2024-06-09 18:03:25 +0200

    Adding navbar and header file

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,15 +4,19 @@ class BlogsController < ApplicationController
   # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "My blogs"
   end
 
   # GET /blogs/1 or /blogs/1.json
   def show
+    @page_title=@blog.title
+    @seo_keywords=@blog.body
   end
 
   # GET /blogs/new
   def new
     @blog = Blog.new
+
   end
 
   # GET /blogs/1/edit
